1a90b62503c95bc9f5b12cb971853c06a7b640eb
Andyrei02 <acenusa58@gmail.com>
2025-02-20 21:33:04 +0200

    delete rotate

--- raspberry/TFT_1_8_rgb/main.py
+++ raspberry/TFT_1_8_rgb/main.py
@@ -16,7 +16,7 @@ tft_rst = board.D25  # Reset
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=tft_rst)
 
 # Initialize Display
-display = ST7735R(display_bus, width=128, height=160, rotation=90)  # Rotation 90 if needed
+display = ST7735R(display_bus, width=128, height=160)  # Rotation 90 if needed
 
 # Create Display Context
 splash = displayio.Group()
